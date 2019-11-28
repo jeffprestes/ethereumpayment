18295a59a8d3c1199a03eef924099b5cb4b4ccba
Jeff Prestes <jeffprestes@gmail.com>
2019-11-28 18:21:00 -0300

    Add files via upload

---
+++ contract/escrow.sol
@@ -0,0 +1,26 @@
+pragma solidity 0.5.13;
+
+contract Escrow {
+    
+    address payable public beneficiary;
+    
+    constructor() public {
+        beneficiary = msg.sender;
+    }
+    
+    function getContractBalance() public view returns(uint256) {
+        return address(this).balance;
+    }
+    
+    function pay() public payable returns(bool) {
+        return true;
+    }
+    
+    function withdraw() public returns(bool) {
+        require(msg.sender==beneficiary, "only beneficiary is allowed to withdraw ethers");
+        require(address(this).balance>0, "there is no balance in this contract");
+        beneficiary.transfer(address(this).balance);
+        return true;
+    }
+    
+}
