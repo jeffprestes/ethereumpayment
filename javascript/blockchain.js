399147df08b80bedd683d004c0253036b5118de1
Jeff Prestes <jeffprestes@gmail.com>
2019-11-28 18:26:30 -0300

    Add files via upload

--- javascript/blockchain.js
+++ javascript/blockchain.js
@@ -1,8 +1,3 @@
-if (!Web3) {
-    alert("A screen will be load asking to allow this page to connect with your Ethereum account.\nPlease give this permission to proceed.\nOr if you don't have an Ethereum account please install Metamask");
-    ethereum.enable();
-    document.location = "index.html";
-}
 var contractAddress = "0x32Dd0823bEAe1434527815b91d7BD5BB13032dFD";
 var provider = new ethers.providers.Web3Provider(web3.currentProvider);
 var signer = provider.getSigner();
@@ -16,6 +11,8 @@ function getContractBalance() {
     })
     .catch( (err) => {
         console.error(err);
-        boxBalance.innerHTML = err;
+        alert("A screen will be load asking to allow this page to connect with your Ethereum account.\nPlease give this permission to proceed.\nOr if you don't have an Ethereum account please install Metamask");
+        ethereum.enable();
+        document.location = "index.html";
     });
 }
