b0fbea5b443dfe7798903f58cc918f5ecaa726e7
jeffprestes <jeffprestes@gmail.com>
2019-12-04 17:12:23 -0300

    new connection methods

--- javascript/blockchain.js
+++ javascript/blockchain.js
@@ -1,7 +1,59 @@
 var contractAddress = "0x557D0Ecd8A9ae210519d1bD9B543723E04941eEe";
-var provider = new ethers.providers.Web3Provider(web3.currentProvider);
-var signer = provider.getSigner();
-var contract = new ethers.Contract(contractAddress, contractAbi, signer);
+var providerDefault = ethers.getDefaultProvider('rinkeby');
+var providerSigner;
+var signer;
+var contract = new ethers.Contract(contractAddress, contractAbi, providerDefault);
+var contractWithSigner;
+var currentAccount;
+
+if (!ethereum || !ethereum.isMetaMask) {
+    alert('Please install MetaMask.');
+}
+
+function loading() {
+    ethereum.send('eth_accounts')
+    .then(handleAccountsChanged)
+    .catch(err => {
+        // In the future, maybe in 2020, this will return a 4100 error if
+        // the user has yet to connect
+        if (err.code === 4100) { // EIP 1193 unauthorized error
+            console.log('Please connect to MetaMask.')
+        } else {
+            console.error(err)
+        }
+    }) 
+}
+
+ethereum.on('accountsChanged', handleAccountsChanged)
+
+function handleAccountsChanged (accounts) {
+    console.log('handleAccountsChanged', accounts.length);
+    providerSigner = new ethers.providers.Web3Provider(web3.currentProvider);
+    signer = providerSigner.getSigner();
+    contractWithSigner = new ethers.Contract(contractAddress, contractAbi, signer);
+    if (accounts.length === 0) {
+        // MetaMask is locked or the user has not connected any accounts
+        console.log('Please connect to MetaMask.')        
+    } else if (accounts[0] !== currentAccount) {  
+        currentAccount = accounts[0];        
+        if (currentAccount) {            
+            console.log('handleAccountsChanged objects', accounts, currentAccount, signer);
+        }
+    }
+}
+
+function connectToWeb3() {  
+    console.log('connectToWeb3 called');
+    ethereum.send('eth_requestAccounts')
+    .then(handleAccountsChanged)
+    .catch(err => {
+      if (err.code === 4001) { // EIP 1193 userRejectedRequest error
+        console.log('Please connect to MetaMask.')
+      } else {
+        console.error(err)
+      }
+    })    
+}
 
 function getContractBalance() {    
     var boxBalance = document.getElementById("boxBalance");
@@ -30,7 +82,7 @@ function executePayment() {
     var additionalSettings = {
         value: ethers.utils.parseUnits(amount, 'wei')
     }; 
-    contract.pay(motivation, additionalSettings)
+    contractWithSigner.pay(motivation, additionalSettings)
     .then( (tx) => {
         console.log("executePayment - Transaction ", tx);   
         boxCommStatus.innerHTML = "Transaction sent. Waiting for the result...";
